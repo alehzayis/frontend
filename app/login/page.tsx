cd9d1cbcd5e24cc840416367a3ca7a4404d9e4e7
Samiramrullah <samiramrullah@gmail.com>
2026-09-01 02:57:57 +0530

    account updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -73,7 +73,7 @@ export default function LoginPage() {
         ? "/change-password"
         : user?.role === "admin"
         ? "/admin"
-        : "/dashboard";
+        : "/account";
 
       router.push(destination);
     } catch (err: any) {
